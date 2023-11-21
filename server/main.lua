d7b4c072e689d2e1d66b0675721a96b4ab08c7ab
The Stoic Bear <112611821+TheStoicBear@users.noreply.github.com>
2023-11-20 16:56:32 -0800

    Update main.lua

--- server/main.lua
+++ server/main.lua
@@ -17,3 +17,43 @@ AddEventHandler('codex-sound:server:PlayWithinDistance', function(maxDistance, s
         print(('[codex-sound] [^3WARNING^7] %s attempted to trigger codex-sound:server:PlayWithinDistance over the distance limit ' .. DistanceLimit):format(GetPlayerName(src)))
     end
 end)
+
+RegisterNetEvent('mpsounds_CL:PlayWithinDistance')
+AddEventHandler('mpsounds_CL:PlayWithinDistance', function(maxDistance, soundFile, soundVolume)
+  if GetConvar("onesync_enableInfinity", "false") == "true" then
+    TriggerClientEvent('mpsounds_CL:PlayWithinDistanceOS', -1, GetEntityCoords(GetPlayerPed(source)), maxDistance, soundFile, soundVolume)
+  else
+    TriggerClientEvent('mpsounds_CL:PlayWithinDistance', -1, source, maxDistance, soundFile, soundVolume)
+  end
+end)
+
+RegisterNetEvent('mpsounds_CL:PlayWithinDistance')
+AddEventHandler('mpsounds_CL:PlayWithinDistance', function(maxDistance, soundFile, soundVolume)
+    local src = source
+    local DistanceLimit = 300
+    if maxDistance < DistanceLimit then
+	TriggerClientEvent('mpsounds_CL:PlayWithinDistance', -1, GetEntityCoords(GetPlayerPed(src)), maxDistance, soundFile, soundVolume)
+    else
+        print(('[interact-sound] [^3WARNING^7] %s attempted to trigger mpsounds_CL:PlayWithinDistance over the distance limit ' .. DistanceLimit):format(GetPlayerName(src)))
+    end
+end)
+
+RegisterNetEvent('SBSound_SV:PlayWithinDistance')
+AddEventHandler('SBSound_SV:PlayWithinDistance', function(maxDistance, soundFile, soundVolume)
+  if GetConvar("onesync_enableInfinity", "false") == "true" then
+    TriggerClientEvent('SBSound_CL:PlayWithinDistanceOS', -1, GetEntityCoords(GetPlayerPed(source)), maxDistance, soundFile, soundVolume)
+  else
+    TriggerClientEvent('SBSound_CL:PlayWithinDistance', -1, source, maxDistance, soundFile, soundVolume)
+  end
+end)
+
+RegisterNetEvent('SBSound_SV:PlayWithinDistance')
+AddEventHandler('SBSound_SV:PlayWithinDistance', function(maxDistance, soundFile, soundVolume)
+    local src = source
+    local DistanceLimit = 300
+    if maxDistance < DistanceLimit then
+	TriggerClientEvent('SBSound_CL:PlayWithinDistance', -1, GetEntityCoords(GetPlayerPed(src)), maxDistance, soundFile, soundVolume)
+    else
+        print(('[interact-sound] [^3WARNING^7] %s attempted to trigger SBSound_SV:PlayWithinDistance over the distance limit ' .. DistanceLimit):format(GetPlayerName(src)))
+    end
+end)
